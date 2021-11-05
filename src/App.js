50b01f5ed555f7dffd7c42017c2c0da65e77c476
unknown <kinchoily@hotmail.com>
2021-11-05 16:53:07 +0000

    App.js - add useEffect for handling PUT request, for sending cart data to firebase database.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import { Fragment } from 'react';
-import { useSelector } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { uiActions } from './store/ui-slice';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notfication';
+
+//define outside the component function, so this doesnt change and reinitialized if the component re renders
+let isInitial = true;
 
 function App() {
 
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);//gets hold of overall cart
+  const notification = useSelector((state) => state.ui.notification);
+  
+  //send http request when cart changes
+  useEffect(() => {
+    const sendCartDate = async () => {
+      //Handling notifications
+      dispatch(uiActions.showNotification({
+        status: 'Pending',
+        title: 'Sending...',
+        message: 'Sending cart data.'
+      }));
+      //Fetch endpoint
+      const response = await fetch(
+        "https://redux-backend-bbe09-default-rtdb.europe-west1.firebasedatabase.app/cart",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart), //stringify cart
+        }
+      );
+      
+      if(!response.ok){
+        throw new Error('Sending Cart Data Failed.')
+      }
+      const responseData = await response.json();
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Sent',
+        message: 'Cart data sent succesfully!'
+      }));
+    };
+
+    if(isInitial){
+      isInitial=false;
+      return; 
+    };
+
+    sendCartDate().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error',
+        message: 'Sending cart data failed!'
+      }));
+    });
+  },[cart, dispatch]);
 
   return (
     <Fragment>
