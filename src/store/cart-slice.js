610e33d54a0c2fc747d9b83c2cc79d4b85f698b5
unknown <kinchoily@hotmail.com>
2021-11-08 18:50:35 +0000

    cart-slice.js - move thunk function into separate file (cart-actions.js)

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { uiActions } from "./ui-slice";
 // ======================================= Notes =======================================
 // Action creator - is a function that literally creates an action object
 // Thunk - Redux Thunk is a middleware that lets you call action creators that return a FUNCTION
@@ -55,60 +54,6 @@ const cartSlice = createSlice({
     },
   },
 });
-
-//Redux thunk function 
-export const sendCartData = (cart) => {
-  return async (dispatch) => {
-    //dispatch update notification (return new object)
-    dispatch(
-      uiActions.showNotification({
-        status: "Pending",
-        title: "Sending...",
-        message: "Sending cart data.",
-      })
-    );
-
-    //function for making a PUT request
-    const sendRequest = async () => {
-      const response = await fetch(
-        "https://redux-backend-bbe09-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart), //stringify cart
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending Cart Data Failed.");
-      }
-    };
-
-    //try sendRequest function
-    try {
-      await sendRequest();
-      //if above function completes with no errors then,
-      //dispatch and update notifications
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Sent",
-          message: "Cart data sent succesfully!",
-        })
-      );
-
-      //if sendRequest function fails
-    } catch (error) {
-      //dispatch error notification
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Sending cart data failed!",
-        })
-      );
-    }
-  };
-};
 
 export const cartActions = cartSlice.actions; //exporting our cartSlice actions 
 export default cartSlice;
